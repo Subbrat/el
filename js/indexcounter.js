87bb43f357cce9280b42f9827aa339667f25bd88
Subrat Mallick <m.subrat198470@gmail.com>
2022-06-01 09:40:32 +0530

    sadfs

--- js/indexcounter.js
+++ js/indexcounter.js
@@ -3,7 +3,7 @@
         updateVisitCount();
 
         function updateVisitCount() {
-                fetch('https://api.countapi.xyz/update/subbrat/key/?amount=+1')
+                fetch('https://api.countapi.xyz/update/subbrat/experimente/?amount=+1')
                         .then(res => res.json())
                         .then(res => {
                                 countE0.innerHTML = res.value;
@@ -16,7 +16,7 @@
         updateVisitCount();
 
         function updateVisitCount() {
-                fetch('https://api.countapi.xyz/get/subbrat/key')
+                fetch('https://api.countapi.xyz/get/subbrat/experimente')
                         .then(res => res.json())
                         .then(res => {
                                 E1.innerHTML = res.value;
